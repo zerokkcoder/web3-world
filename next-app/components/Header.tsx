70aa849edde4b67f88ecca3ac7846c149c45b194
huanggz <918031078@qq.com>
2022-12-15 10:49:19 +0800

    modify header

--- next-app/components/Header.tsx
+++ next-app/components/Header.tsx
@@ -6,7 +6,10 @@ export default function Header() {
     return (
         <header className="container mx-auto flex justify-between h-16 items-center">
             <Link href="/">
-                <Image className="rounded-full" src="/logo.jpg" alt="logo" width="36" height="36"/>
+                <div className="flex items-center gap-2 font-medium">
+                    <Image className="rounded-full" src="/logo.jpg" alt="logo" width="36" height="36"/>
+                    <span>ZeroDot618</span>
+                </div>
             </Link>
             <nav>
                 <ul className="flex gap-6 font-medium items-center">
